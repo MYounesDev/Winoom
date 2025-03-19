a967443c1869c9a884fcd873dd2a358490dc2882
MYounesEG <mohammadyounes686@gmail.com>
2025-03-19 20:42:51 +0300

    add setup.bat

---
+++ setup.bat
@@ -0,0 +1,1 @@
+cd server && npm install cors dotenv express pg react-router-dom && npm install --save-dev nodemon && cd ..  && cd Client && npm install && npm install axios lucide-react next@15.2.2 react@19.0.0 react-dom@19.0.0 react-router-dom && npm install --save-dev @tailwindcss/postcss @types/node @types/react @types/react-dom tailwindcss typescript
